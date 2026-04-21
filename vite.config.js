3b6bc3384ad12c65a89c6fdb44463e9ec1e350c4
Soyeon0123 <switle247@naver.com>
2026-04-21 06:37:27 -0400

    rebuild-third

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command } ) => ({  plugins: [react()],
   base: command === 'build' ? '/Rootin-app/' : '/',
+  build: {
+    outDir: 'docs',
+  },
 }))
